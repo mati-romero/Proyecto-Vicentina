12ef446445e77254115940ff1846a50ed6bfa308
Romero <matias.1993.romero@gmail.com>
2021-11-02 23:54:25 -0300

    Nueva version de la pagina

--- js/compartir.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$(".btn-compartir").on('click',function() {
-
-		var associated =  $('#'+ $(".btn-compartir").attr('associated'));
-		associated.fadeToggle("1000")
-	});
-});
